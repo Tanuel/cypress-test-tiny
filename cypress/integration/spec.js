bc6750f016c7cdf813318df12ca5f4be1acfc511
Tanuel <tanuel.mategi@gmail.com>
2020-10-27 11:54:26 +0100

    implement recaptcha test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('localhost:8080');
+    cy.wait(4000);
+    cy.get('#logger').should('have.text', 'captcha success')
   })
 })
